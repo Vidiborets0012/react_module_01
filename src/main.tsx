a20eb23dad767872e74900e741cb553c22175957
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-10-27 18:44:23 +0200

    add a topic: components

--- src/main.tsx
+++ src/main.tsx
@@ -17,10 +17,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
