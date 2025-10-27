35c895181506f54103b8e865314ce8297a3f9983
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-10-27 19:02:44 +0200

    add a topic: component properties

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -45,14 +45,32 @@
 
 import Product from './Product';
 
+// export default function App() {
+//   return (
+//     <>
+//       <h1>Products</h1>
+
+//       <Product />
+//       <Product />
+//       <Product />
+//     </>
+//   );
+// }
+
 export default function App() {
   return (
     <>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
     </>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,8 +1,34 @@
-export default function Product() {
+// export default function Product() {
+//   return (
+//     <div>
+//       <h2>Tacos</h2>
+//       <p>Price: 999 credits</p>
+//     </div>
+//   );
+// }
+interface ProductProps {
+  name: string;
+  imgUrl: string;
+  price: number;
+}
+
+// export default function Product(props: ProductProps) {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <img src={props.imgUrl} alt={props.name} width="320" />
+//       <p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// }
+
+//Деструктуризація props
+export default function Product({ name, imgUrl, price }: ProductProps) {
   return (
     <div>
-      <h2>Tacos</h2>
-      <p>Price: 999 credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="320" />
+      <p>Price: {price} credits</p>
     </div>
   );
 }
